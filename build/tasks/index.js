b1c9b37ba3836678af55bdaaea55c1801cf3bc48
Marina Tatic <tatic.marina8@gmail.com>
2021-05-10 21:31:09 +0200

    Homepage added.

---
+++ build/tasks/index.js
@@ -0,0 +1,6 @@
+export * from './assets';
+export * from './css';
+export * from './files';
+export * from './html';
+export * from './js';
+export * from './svg-sprite';
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Main JS
+ *
+ * @copyright 2021 NB Communication Ltd
+ *
+ */
+
+const main = {
+
+	init: () => {
+        ImageMap('img[usemap]')
+	}
+};
+
+main.init();
